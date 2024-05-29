d33d071c6fba124f71dae7c7961ba09b31eaee15
sagun <sagun@DESKTOP-Q0BH37P>
2024-05-30 00:03:02 +0530

    Java Program

---
+++ codecafe/src/day2classwork/forloop.java
@@ -0,0 +1,18 @@
+package day2classwork;
+
+public class forloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		{
+			for(int i=1;i<=1000;i++)
+			{
+				System.out.println("Value of i:");
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
